d7a2c1ce754cacfda996cc203d2f8c3c978fc3ac
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2024-07-27 18:25:19 +0530

    About name change

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import About from "@/components/About";
+import Secure from "@/components/Secure";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <About />
+      <Secure />
     </main>
   );
 }
